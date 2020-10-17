a0faaf30b29d2e7b02a497d4a031c3f25090f4a8
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2020-10-17 23:30:19 +0900

    chore: setup Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    content: ["./src/**/*.vue"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
