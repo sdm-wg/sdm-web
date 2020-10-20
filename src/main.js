904e8c1ff322c13d1eff3396ea2a5128007f9e37
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2020-10-20 13:03:55 +0900

    chore: add Font Awesome

--- src/main.js
+++ src/main.js
@@ -5,9 +5,19 @@ import "~/assets/css/tailwind.css";
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
+// import: Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {} from "@fortawesome/free-solid-svg-icons";
+import {} from "@fortawesome/free-regular-svg-icons";
+import {} from "@fortawesome/free-brands-svg-icons";
+
 import DefaultLayout from "~/layouts/Default.vue";
 
+// Font Awesome setting
+library.add();
+
 export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
+  Vue.component("font-awesome", FontAwesomeIcon);
 }
