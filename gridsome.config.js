227948d067e5761d8683bf7ac92ffcb5f3a0e788
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2020-10-20 12:14:55 +0900

    feat: update gridsome config

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,11 +1,9 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
-  siteName: "Gridsome",
+  siteName: "Software Defined Media Consortium",
+  siteDescription: "",
+  siteUrl: "https://sdm.wide.ad.jp/",
+  pathPrefix: "",
+  titleTemplate: "%s | <siteName>",
   plugins: [
     {
       use: "@gridsome/vue-remark",
