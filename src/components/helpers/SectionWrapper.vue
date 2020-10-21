70c82e2ecefbb6c840f50f4b60c92095136293ad
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2020-10-21 23:03:05 +0900

    feat: add section wrapper

---
+++ src/components/helpers/SectionWrapper.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="w-full py-4">
+    <section :id="sectionId" class="flex flex-wrap w-full container mx-auto">
+      <div class="w-full">
+        <h1
+          class="font-bold text-center text-4xl sm:text-5xl md:text-6xl transition-set"
+          :class="{
+            'text-gray-100': isDark,
+            'text-gray-900': !isDark,
+          }"
+        >
+          <slot name="heading" />
+        </h1>
+      </div>
+      <slot />
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionWrapper",
+  props: {
+    isDark: {
+      type: Boolean,
+      required: true,
+    },
+    sectionId: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
