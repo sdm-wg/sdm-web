96a44ed3d04306c58323eebdf64b9bec1faffdf7
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2020-10-22 23:10:26 +0900

    fix: add process.browser condition

--- src/utils/unitConverter.js
+++ src/utils/unitConverter.js
@@ -1,4 +1,7 @@
 export const rem2px = (rem) => {
-  const fontSize = getComputedStyle(document.documentElement).fontSize;
-  return rem * parseFloat(fontSize);
+  if (process.browser) {
+    const fontSize = getComputedStyle(document.documentElement).fontSize;
+    return rem * parseFloat(fontSize);
+  }
+  return rem;
 };
