4329983e8f5fb06a57203726601901b91d44a186
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2020-10-25 22:35:57 +0900

    feat: add post search

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -2,12 +2,45 @@
   <Layout @receive-toggle-dark="toggleDark" :isDark="isDark">
     <SectionWrapper :isDark="isDark" :isEven="true" sectionId="tag">
       <template v-slot:heading>{{ heading }}</template>
-      <PostCard
-        v-for="post in $page.tag.belongsTo.edges"
-        :key="post.node.id"
-        :isDark="isDark"
-        :post="post"
-      />
+      <!-- Media Info -->
+      <div class="flex flex-col w-full">
+        <!-- Search -->
+        <div class="flex flex-row-reverse w-full px-4 pb-4">
+          <input
+            v-model="searchText"
+            class="w-full md:w-64 p-1 border border-gray-400 rounded-lg appearance-none focus:outline-none active:outline-none text-gray-900"
+            :class="{
+              'focus:border-blue-500 active:border-blue-500 bg-gray-100': isDark,
+              'focus:border-orange-500 active:border-orange-500': !isDark,
+            }"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+
+        <!-- Post Cards -->
+        <div class="flex flex-row flex-wrap">
+          <PostCard
+            v-for="post in filteredPost"
+            :key="post.node.id"
+            :isDark="isDark"
+            :post="post"
+          />
+        </div>
+
+        <!-- Entry Count -->
+        <div class="px-4 pt-4">
+          <span
+            class="text-base transition-set"
+            :class="{
+              'text-gray-100': isDark,
+              'text-gray-900': !isDark,
+            }"
+          >
+            Showing {{ filteredPostLength }} out of {{ postLength }} entries
+          </span>
+        </div>
+      </div>
     </SectionWrapper>
   </Layout>
 </template>
@@ -65,9 +98,33 @@ export default {
     };
   },
   mixins: [localeMixin, colorSchemeMixin],
+  data: () => {
+    return {
+      searchText: "",
+    };
+  },
   computed: {
     heading: function () {
       return unslug(this.$page.tag.id);
+    },
+    filteredPost: function () {
+      const keyword = this.searchText.toLowerCase();
+      return this.$page.tag.belongsTo.edges.filter((post) => {
+        const { title, summary, tags, date } = post.node;
+        const joinedTag = tags.map((node) => node.id).join(" ");
+        return (
+          title[this.language].toLowerCase().includes(keyword) ||
+          summary[this.language].toLowerCase().includes(keyword) ||
+          joinedTag.toLowerCase().includes(keyword) ||
+          date.toLowerCase().includes(keyword)
+        );
+      });
+    },
+    postLength: function () {
+      return this.$page.tag.belongsTo.edges.length;
+    },
+    filteredPostLength: function () {
+      return this.filteredPost.length;
     },
   },
   components: {
